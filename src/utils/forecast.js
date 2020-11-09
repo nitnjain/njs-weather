d62b8944ca7a1543e1d54e7859a20afa05ad397d
nitnjain <nitinfyi@gmail.com>
2020-11-09 10:20:07 +0530

    added humididty to data

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -11,7 +11,7 @@ const forecast = (lat, long, callback) => {
         } else if(body.error) {
             callback(`Unable to find location`, undefined);
         } else {
-            callback(undefined, `${body.current.weather_descriptions[0]}! The temparature is ${body.current.temperature} out. It feels like ${body.current.feelslike}`);
+            callback(undefined, `${body.current.weather_descriptions[0]}! The temparature is ${body.current.temperature} out. It feels like ${body.current.feelslike}. Humidity is ${body.current.humidity}`);
         }
     })
 }
